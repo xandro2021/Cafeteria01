b8cc23f7957d19e147abe4e8f11080b83616b2a1
xandro2021 <xandrocastro2021@gmail.com>
2022-11-27 17:09:07 -0600

    Adding images and creating webp version of them using Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 const { src, dest, watch, series, parallel } = require( "gulp" );
+
+// CSS y SASS
 const sass = require( "gulp-sass" )( require( "sass" ) );
 const postcss = require( "gulp-postcss" );
 const autoprefixer = require( "autoprefixer" );
+
+// IMAGENES
+const imagemin = require( "gulp-imagemin" );
+const webp = require( "gulp-webp" );
 
 // Funcion que compila y guarda los cambios en el archivo
 function css( done ) {
@@ -16,14 +22,33 @@ function css( done ) {
   done();
 }
 
+function imagenes ( done ) {
+  src( "src/img/**/*" )
+    .pipe( imagemin({ optimizationLevel: 3 }) )
+    .pipe( dest( "build/img" ) );
+
+  done();
+}
+
+function versionWebp( done ) {
+  src( "src/img/**/*.{png,jpg}" )
+    .pipe( webp() )
+    .pipe( dest( "build/img" ) );
+
+  done();
+}
+
 // Funcion que esta a la escucha de los cambios
 function dev() {
   watch( "src/scss/**/*.scss", css );
+  watch( "src/img/**/*", imagenes );
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series( css, dev );
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.default = series( imagenes, versionWebp, css, dev );
 
 // Series - Se inicia una tarea y hasta que finaliza se inicia la siguiente
 // Parallel - Todas inician al mismo tiempo y se completan de forma diferente
