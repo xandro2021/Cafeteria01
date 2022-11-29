223d6d92fefb9632a37ceaddb2671a9d0e580a6a
xandro2021 <xandrocastro2021@gmail.com>
2022-11-28 20:16:05 -0600

    Creating images avif and webp. Learning more about working with SASS

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const autoprefixer = require( "autoprefixer" );
 // IMAGENES
 const imagemin = require( "gulp-imagemin" );
 const webp = require( "gulp-webp" );
+const avif = require( "gulp-avif" );
 
 // Funcion que compila y guarda los cambios en el archivo
 function css( done ) {
@@ -31,11 +32,24 @@ function imagenes ( done ) {
 }
 
 function versionWebp( done ) {
+  const opciones = {
+    quality: 50
+  }
   src( "src/img/**/*.{png,jpg}" )
-    .pipe( webp() )
+    .pipe( webp( opciones ) )
     .pipe( dest( "build/img" ) );
 
   done();
+}
+
+function versionAvif() {
+  const opciones = {
+    quality: 50
+  }
+
+  return src( "src/img/**/*.{png,jpg}" )
+    .pipe( avif( opciones ) )
+    .pipe( dest("build/img") );
 }
 
 // Funcion que esta a la escucha de los cambios
@@ -48,7 +62,8 @@ exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.default = series( imagenes, versionWebp, css, dev );
+exports.versionAvif = versionAvif;
+exports.default = series( imagenes, versionWebp, versionAvif, css, dev );
 
 // Series - Se inicia una tarea y hasta que finaliza se inicia la siguiente
 // Parallel - Todas inician al mismo tiempo y se completan de forma diferente
